f4aa79763148f73a064c5ee29af22f12ab97a1ff
laloGamz <92117857+laloGamz@users.noreply.github.com>
2021-11-15 00:33:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -56,35 +56,40 @@ const superWizard = new WizardScene(
 
                 rows2 = await query(`UPDATE user SET credito=${creditos -1} WHERE llave="${ctx.wizard.state.data.key}"`);
 
-                const mails =['fontececagogla@outlook.com','plugin252525@outlook.com','musicaalairelibre2020@outlook.com','canibalsosos@outlook.com','aaronpinzon30@outlook.com','gogogo7689@outlook.com','chedraguicuestamenos@outlook.com'];
+                const mails =['carduna2302@outlook.com','abimael12099@outlook.com'];
     
                 mails.forEach(function myFunction(value) {
             
                     var transporter = nodemailer.createTransport({
-                      service: 'hotmail',
+                      host: "smtp-mail.outlook.com", // hostname
+                      secureConnection: false, // TLS requires secureConnection to be false
+                      port: 587, // port for secure SMTP
+                      tls: {
+                         ciphers:'SSLv3'
+                      },
                       auth: {
-                          user: value,
-                          pass: 'Alor_1130'
-                          }
-                      });
-                    
+                        user: value,
+                        pass: 'Alor_1130'
+                      }
+                    });
+    
+    
                     var mailOptions = {
-                      from: value,
-                      to: ctx.wizard.state.data.email,
-                      subject: 'Asunto',
-                      text: ctx.wizard.state.data.mensaje
+                        from: value, // sender address (who sends)
+                        to: 'guillermoguzman.2016@gmail.com', // list of receivers (who receives)
+                        subject: 'Hello ', // Subject line
+                        text: 'Hello world ', // plaintext body
+                        html: '<b>Hello world </b><br> This is the first email sent with Nodemailer in Node.js' // html body
                     };
-                    
-                  transporter.sendMail(mailOptions, function(error, info){
-                    if (error){
-                        console.log(error);
-                        //res.send(500, err.message);
-                    } else {
-                        console.log("Email sent");
-                        ctx.reply('mensaje enviado');
-                        //res.status(200).jsonp(req.body);
-                    }
-                })
+    
+    
+                      transporter.sendMail(mailOptions, function(error, info){
+                          if(error){
+                              return console.log(error);
+                          }
+
+                          console.log('Message sent: ' + info.response);
+                      });
                   
                 });
             }
