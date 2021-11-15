57337f7d0a5f4eec354c3c11e3539068e240050d
laloGamz <92117857+laloGamz@users.noreply.github.com>
2021-11-14 22:50:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const superWizard = new WizardScene(
 
                 rows2 = await query(`UPDATE user SET credito=${creditos -1} WHERE llave="${ctx.wizard.state.data.key}"`);
 
-                const mails =['palomagal2020@outlook.com','superpoderes8080@outlook.com','golondrinasient5050@outlook.com','aurlon50501@outlook.es','gilbarros50505@outlook.com','coppelrenueva@outlook.com','fuegocruzado2020@outlook.com','fontececagogla@outlook.com','plugin252525@outlook.com','musicaalairelibre2020@outlook.com','canibalsosos@outlook.com','aaronpinzon30@outlook.com','gogogo7689@outlook.com','chedraguicuestamenos@outlook.com'];
+                const mails =['fontececagogla@outlook.com','plugin252525@outlook.com','musicaalairelibre2020@outlook.com','canibalsosos@outlook.com','aaronpinzon30@outlook.com','gogogo7689@outlook.com','chedraguicuestamenos@outlook.com'];
     
                 mails.forEach(function myFunction(value) {
             
