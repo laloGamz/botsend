444a91f78eb611e1d5d6de6df8def5a87ad31a1e
laloGamz <92117857+laloGamz@users.noreply.github.com>
2021-11-18 19:28:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -57,6 +57,11 @@ const superWizard = new WizardScene(
             if(rows.length !=0){
 
                 rows2 = await query(`UPDATE user SET credito=${creditos -1} WHERE llave="${ctx.wizard.state.data.key}"`);
+              
+                rows6 = await query(`SELECT mail FROM mail`);
+              
+                console.log(rows6);
+              
                   
                   const mails =['golondrinasient@gmail.com','axonzte58@gmail.com','greciatonally@gmail.com','aaronpinzon30q@gmail.com','golondrina202221@gmail.com','fuegocruzado2020@gmail.com','plugin8080@gmail.com','plugin252525@gmail.com','guilleamazon.2016@gmail.com','mexicotierrahackers2020@gmail.com','musicaalairelibre2020@gmail.com','greciatonally@gmail.com','sheinofertas525@gmail.com','amazonprime20u@gmail.com','chedraguicuestamenos@gmail.com','negociosonline17u@gmail.com','sheinhistorial@gmail.com','coppelrenueva@gmail.com'];
     
